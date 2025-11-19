c4765044a9a83b09428da71ea3806023a1b652b2
gaby <gabriela.gutierrezr@udg.mx>
2025-11-19 14:31:53 -0600

    Valid Parentheses

---
+++ NeetCode/src/main/java/stack/ValidParentheses.java
@@ -0,0 +1,45 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "d";
+        System.out.println("Resultado: " + isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> pila = new Stack<>();
+        char[] c = s.toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+            switch (c[i]) {
+                case '(', '[', '{' -> {
+                    pila.push(c[i]);
+                }
+                case ')', ']', '}' -> {
+                    if (pila.isEmpty()) {
+                        return false;
+                    } else {
+                        if (apertura(c[i]) != pila.pop()) {
+                            return false;
+                        }
+                    }
+                }
+                default -> {return false;}
+            }
+        }
+        if (pila.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static char apertura(char caracter) {
+        if (caracter == '}') return '{';
+        else if (caracter == ']') return '[';
+        else if (caracter == ')') return '(';
+        else return 'x';
+    }
+}
